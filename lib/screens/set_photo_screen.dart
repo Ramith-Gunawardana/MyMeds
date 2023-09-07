19d26e29cce2d940518441e6add5ad8122d7d7b1
Pubudu Ashan Sankalpa <pubuduasankalpa4566@gmail.com>
2023-09-07 11:30:47 +0530

    wrong

--- lib/screens/set_photo_screen.dart
+++ lib/screens/set_photo_screen.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:mymeds_app/screens/settings.dart';
-import '../components/common_buttons.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../components/common_buttons.dart';
 import '../components/constants.dart';
 import 'select_photo_options_screen.dart';
 
-// ignore: must_be_immutable
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
 class SetPhotoScreen extends StatefulWidget {
   const SetPhotoScreen({super.key});
 
@@ -20,28 +24,98 @@ class SetPhotoScreen extends StatefulWidget {
 }
 
 class _SetPhotoScreenState extends State<SetPhotoScreen> {
-  File? _image;
+  String buttonText = 'Save Photo';
+  bool showIcon = false;
+  String? imageUrl;
+   File? _image;
 
-  Future <void>_pickImage(ImageSource source) async{
-    try{
-    final image = await ImagePicker().pickImage(source: source);
-    if (image == null)return;
-    File? img = File(image.path);
-    img = await _cropImage(imageFile: img);
-    setState(() {
-      _image =img;
+  void initState() {
+    super.initState();
+    _loadSavedImage();
+    Firebase.initializeApp();
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      File? img = File(image.path);
+      img = await _cropImage(imageFile: img);
+
+      if (img != null) {
+        // await _saveImage(img);
+        await uploadImage(img);
+        setState(() {
+          _image = img;
+          Navigator.of(context).pop();
+        });
+      }
+      // setState(() {
+      //   _image =img;
+      //   Navigator.of(context).pop();
+      // });
+    } on PlatformException catch (e) {
+      print(e);
       Navigator.of(context).pop();
-    });
-  } on PlatformException catch (e){
-    print(e);
-    Navigator.of(context).pop();
-  }
+    }
   }
 
-  Future<File?> _cropImage({required File imageFile}) async{
-    CroppedFile? croppedImage = 
-    await ImageCropper().cropImage(sourcePath: imageFile.path);
-    if(croppedImage == null)return null;
+  Future<void> uploadImage(File imageFile) async {
+    try {
+      final storageReference = firebase_storage
+          .FirebaseStorage.instance
+          .ref()
+          .child('images/${DateTime.now().millisecondsSinceEpoch}.jpg');
+      final UploadTask uploadTask = storageReference.putFile(imageFile);
+      final TaskSnapshot downloadTaskSnapshot = await uploadTask;
+      final String url = await downloadTaskSnapshot.ref.getDownloadURL();
+
+      // Update the state to store the image URL
+      setState(() {
+        imageUrl = url;
+        buttonText = 'Photo Uploaded';
+        showIcon = true;
+      });
+      print('Image uploaded to Firebase Storage');
+    } catch (e) {
+      print('Error uploading image: $e');
+      setState(() {
+        buttonText = 'Error Uploading';
+        showIcon = false;
+      });
+    }
+  }
+
+
+  Future<void> _loadSavedImage() async {
+  final prefs = await SharedPreferences.getInstance();
+  final imagePath = prefs.getString('saved_image_path');
+
+  if (imagePath != null) {
+    // If there's a local image path, load it from Firebase Storage
+    try {
+      final storageReference = firebase_storage
+          .FirebaseStorage.instance
+          .ref()
+          .child('images/image.jpg');
+
+      final url = await storageReference.getDownloadURL();
+
+      setState(() {
+        // Update the _image variable with the downloaded image
+        _image = File(url);
+      });
+    } catch (e) {
+      print('Error loading image from Firebase Storage: $e');
+    }
+  }
+}
+
+
+  Future<File?> _cropImage({required File imageFile}) async {
+    CroppedFile? croppedImage =
+        await ImageCropper().cropImage(sourcePath: imageFile.path);
+    if (croppedImage == null) return null;
     return File(croppedImage.path);
   }
 
@@ -62,8 +136,9 @@ class _SetPhotoScreenState extends State<SetPhotoScreen> {
           builder: (context, scrollController) {
             return SingleChildScrollView(
               controller: scrollController,
-              child:  SelectPhotoOptionsScreen(
-                onTap: _pickImage,),
+              child: SelectPhotoOptionsScreen(
+                onTap: _pickImage,
+              ),
             );
           }),
     );
@@ -73,34 +148,39 @@ class _SetPhotoScreenState extends State<SetPhotoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:const Color.fromARGB(255, 0, 0, 0),
-        title: const Text('Set photo of prescription',
-
-        style: TextStyle(fontSize: 23,color: Colors.white),),
-        leading: IconButton(onPressed: (){
-          Navigator.pop(context,
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        title: const Text(
+          'Set photo of prescription',
+          style: TextStyle(fontSize: 23, color: Colors.white),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(
+              context,
               MaterialPageRoute(
-                builder: (context) =>const AppSettings(),
-                ),);
-        },
-        icon: const Icon(
-          Icons.arrow_back,
-          color: Color.fromARGB(255, 255, 255, 255),
-        ),),),
+                builder: (context) => const AppSettings(),
+              ),
+            );
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding:
-              const EdgeInsets.only(
-                left: 20, right: 20, bottom: 30, top: 20),
+              const EdgeInsets.only(left: 20, right: 20, bottom: 30, top: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-             const Column(
+              const Column(
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children:  [
+                    children: [
                       // SizedBox(
                       //   height: 30,
                       // ),
@@ -132,25 +212,27 @@ class _SetPhotoScreenState extends State<SetPhotoScreen> {
                     },
                     child: Center(
                       child: Container(
-                          width : MediaQuery.of(context).size.width * 1.5, 
-                          height : MediaQuery.of(context).size.width * 1.1, 
+                          width: MediaQuery.of(context).size.width * 1.5,
+                          height: MediaQuery.of(context).size.width * 1.0,
                           decoration: BoxDecoration(
                             shape: BoxShape.rectangle,
                             color: Colors.grey.shade200,
                           ),
-                          child:  Center(
+                          child: Center(
                             child: _image == null
-                            ? const Text(
-                              'No image selected',
-                              style: TextStyle(fontSize: 20),
-                            )
-                            : Image.file(
-                                _image!,
-                                width : MediaQuery.of(context).size.width * 1.5, 
-                                height : MediaQuery.of(context).size.width * 1.1,
-                                fit: BoxFit.fill,
-                              ),
-                            
+                                ? const Text(
+                                    'No image selected',
+                                    style: TextStyle(fontSize: 20),
+                                  )
+                                : Image.file(
+                                    _image!,
+                                    width:
+                                        MediaQuery.of(context).size.width * 1.5,
+                                    height:
+                                        MediaQuery.of(context).size.width * 1.0,
+                                    fit: BoxFit.fill,
+                                  ),
+
                             // :CircleAvatar(
                             //   radius: 100.0,
                             //   backgroundImage: FileImage(_image!),
@@ -183,6 +265,22 @@ class _SetPhotoScreenState extends State<SetPhotoScreen> {
                   ),
                 ],
               ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_image != null) {
+                      _saveImage(_image!);
+                    }
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(buttonText),
+                      if (showIcon) Icon(Icons.check_circle),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
